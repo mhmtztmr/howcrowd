05d35fd5a057c1330452be293b09eebb52a1937a
mhmtztmr <mahmutoztemur@gmail.com>
2016-09-01 19:52:33 +0300

    increase nearby distance to 100m, far distance to 50km

--- www_src/js/service/config/configService.js
+++ www_src/js/service/config/configService.js
@@ -1,7 +1,7 @@
 var configService = function() {
   return {
-    NEARBY_DISTANCE: 0.03, //km
-    FAR_DISTANCE: 15, //km => city
+    NEARBY_DISTANCE: 0.1, //km
+    FAR_DISTANCE: 50, //km => city
     NEARBY_TIME: 1, //hour
     REPORT_EMAIL: 'muhahmut@gmail.com'
   };
